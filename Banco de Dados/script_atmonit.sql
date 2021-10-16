1d97ec83111e32830fed54b25f6d44231e049c12
abnerlucss <abner.santos@bandtec.com.br>
2021-10-16 12:40:04 -0300

    Refatorando o script do banco de dados

--- Banco de Dados/script_atmonit.sql
+++ Banco de Dados/script_atmonit.sql
@@ -1,94 +1,78 @@
-create database atmonit;
+CREATE DATABASE atmonit;
 
-use atmonit;
+USE atmonit;
 
-create table endereco_terminal(
-	id_endereco int primary key auto_increment,
-    logradouro varchar(60),
-    cidade varchar(35) not null,
-    estado varchar(25) not null,
-    bairro varchar(35) not null
+CREATE TABLE IF NOT EXISTS company(
+  id_company INT PRIMARY KEY auto_increment,
+  company_name VARCHAR(150) NOT NULL,
+  cnpj CHAR(14) NOT NULL
 );
 
-create table empresa(
-	id_empresa int primary key auto_increment,
-    nome varchar(50) not null,
-    cnpj char(14) not null
+CREATE TABLE IF NOT EXISTS terminal_address (
+  id_terminal_address INT PRIMARY KEY AUTO_INCREMENT,
+  street VARCHAR(150) NOT NULL,
+  city VARCHAR(150) NOT NULL,
+  state VARCHAR(150) NOT NULL,
+  district VARCHAR(150) NOT NULL
 );
 
-create table funcionario(
-	id_funcionario int primary key auto_increment,
-    nome varchar(100) not null,
-    cargo enum("Administrador", "Gerente de Suporte", "Operação") not null,
-    login varchar(50) unique not null,
-    senha varchar(250) not null,
-    fk_empresa int not null,
-    foreign key(fk_empresa) references empresa(id_empresa)
-);
-
-create table terminal(
-	id_terminal int primary key auto_increment,
-    processador varchar(50),
-    ram varchar(15),
-    armazenamento varchar(15),
-    modelo_cpu varchar(50),
-    hostname varchar(50),
-    fk_endereco int not null,
-    foreign key(fk_endereco) references endereco_terminal(id_endereco),
-    fk_empresa int not null,
-    foreign key(fk_empresa) references empresa(id_empresa)
-);
-
-create table historico_uso(
-	id_historico_uso int primary key auto_increment,
-    tempo_uso time,
-    tempo_desuso time,
-    frequencia_media_uso double,
-    porcentagem_uso_dia double,
-    porcentagem_uso_semana double,
-    porcentagem_uso_mes double,
-    fk_terminal int not null,
-    foreign key(fk_terminal) references terminal(id_terminal)
-);
-
-create table registro_componentes(
-	id_registro_componentes int primary key auto_increment,
-    nome_componente varchar(50) not null, 
-    porcentagem_uso double,
-    data_hora datetime default current_timestamp,
-    frequencia double,
-    temperatura double,
-    fk_terminal int,
-    foreign key(fk_terminal) references terminal (id_terminal)
-);
-
-create table registro_atividade(
-	id_registro_atividade int primary key auto_increment,
-    terminal_ativo boolean,
-    hora_atividade datetime,
-    fk_terminal int,
-    foreign key(fk_terminal) references terminal (id_terminal)
-);
-
-use atmonit;
-select * from empresa;
-
-select * from endereco_terminal;
-
-select * from funcionario;
-
-select * from terminal;
+CREATE TABLE IF NOT EXISTS terminal(
+  id_terminal INT PRIMARY KEY AUTO_INCREMENT,
+  processor VARCHAR(50) NOT NULL,
+  ram_memory VARCHAR(50) NOT NULL,
+  terminal_storage VARCHAR(50) NOT NULL,
+  cpu_model VARCHAR(50) NOT NULL,
+  mac_address VARCHAR(45) NOT NULL,
+  fk_terminal_address INT NOT NULL,
+  fk_company INT NOT NULL,
+  FOREIGN KEY (fk_company) REFERENCES company (id_company),
+  FOREIGN KEY (fk_terminal_address) REFERENCES terminal_address(id_terminal_address)
+ );
  
-select * from registro_componentes;
-select * from funcionario as f join empresa as e on f.fk_empresa = e.id_empresa where e.nome = 'Banco24Horas' and f.login = 'abnin' and f.senha = '123';
+ CREATE TABLE IF NOT EXISTS employee(
+  id_employee INT PRIMARY KEY AUTO_INCREMENT,
+  employee_name VARCHAR(150),
+  office VARCHAR(150) NULL,
+  fk_company INT NOT NULL,
+  login VARCHAR(150) NULL,
+  password VARCHAR(255) NULL,
+  FOREIGN KEY (fk_company) REFERENCES company(id_company)
+);
 
-select * from funcionario;
-select * from empresa;
+CREATE TABLE IF NOT EXISTS component_registration (
+  id_component_registration INT PRIMARY KEY AUTO_INCREMENT,
+  nome_component VARCHAR(150) NOT NULL,
+  percentage_usage DOUBLE NOT NULL,
+  date_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+  frequency DOUBLE NULL,
+  temperature DOUBLE NULL,
+  fk_terminal INT NOT NULL,
+  FOREIGN KEY (fk_terminal) REFERENCES terminal(id_terminal)
+);
 
-desc registro_componentes;
+CREATE TABLE IF NOT EXISTS activity_log (
+  id_activity_log INT PRIMARY KEY AUTO_INCREMENT,
+  active_terminal boolean default false,
+  activity_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  fk_terminal INT NOT NULL,
+  FOREIGN KEY (fk_terminal) REFERENCES terminal(id_terminal)
+);
+
+CREATE TABLE IF NOT EXISTS usage_log (
+  id_usage_log INT PRIMARY KEY AUTO_INCREMENT,
+  fk_terminal INT NOT NULL,
+  average_usage_time TIME NULL,
+  average_disuse_time TIME NULL,
+  usage_frequency DOUBLE NULL,
+  percentage_usage_day DOUBLE NULL,
+  percentage_usage_week DOUBLE NULL,
+  percentage_usage_month DOUBLE NULL,
+  FOREIGN KEY (fk_terminal) REFERENCES terminal(id_terminal)
+);
 
 
-insert into registro_componentes (nome_componente, porcentagem_uso, frequencia, fk_terminal) values (?, ?, ?, ?, ?);
 
-alter table registro_componentes modify data_hora datetime default current_timestamp;
+
+
+
 
